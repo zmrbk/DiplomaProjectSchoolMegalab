223be51009a5fc48c72a25b7dbcb3b822f1c5dd9
Zamir <50745794+zmrbk@users.noreply.github.com>
2024-09-26 19:43:35 +0600

    first commit

---
+++ src/main/java/kg/megacom/diplomaprojectschoolmegalab/DiplomaProjectSchoolMegalabApplication.java
@@ -0,0 +1,13 @@
+package kg.megacom.diplomaprojectschoolmegalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaProjectSchoolMegalabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomaProjectSchoolMegalabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/megacom/diplomaprojectschoolmegalab/DiplomaProjectSchoolMegalabApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megacom.diplomaprojectschoolmegalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaProjectSchoolMegalabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

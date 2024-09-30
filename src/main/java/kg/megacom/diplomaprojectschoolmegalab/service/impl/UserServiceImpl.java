ee9746f1ed1eeb23926259e262beeaf25c66bed9
Admin <armatov.jenisbek@gmail.com>
2024-10-01 00:13:12 +0600

    Add Spring security.

--- src/main/java/kg/megacom/diplomaprojectschoolmegalab/service/impl/UserServiceImpl.java
+++ src/main/java/kg/megacom/diplomaprojectschoolmegalab/service/impl/UserServiceImpl.java
@@ -1,33 +1,23 @@
 package kg.megacom.diplomaprojectschoolmegalab.service.impl;
 
 
-import kg.megacom.diplomaprojectschoolmegalab.dto.Response;
 import kg.megacom.diplomaprojectschoolmegalab.enams.Role;
 import kg.megacom.diplomaprojectschoolmegalab.entity.User;
-import kg.megacom.diplomaprojectschoolmegalab.mappers.UserMapper;
 import kg.megacom.diplomaprojectschoolmegalab.repository.UserRepository;
 import kg.megacom.diplomaprojectschoolmegalab.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepository userRepository;
-    private final UserMapper userMapper;
 
     @Override
     public User create(User user) {

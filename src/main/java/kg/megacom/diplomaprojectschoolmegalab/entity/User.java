e0af6c25bc05c56fe2e0f7b6e12eb2da71ef59b2
user <armatov.jenisbek@gmail.com>
2024-10-03 10:27:49 +0600

    add comment

--- src/main/java/kg/megacom/diplomaprojectschoolmegalab/entity/User.java
+++ src/main/java/kg/megacom/diplomaprojectschoolmegalab/entity/User.java
@@ -22,7 +22,7 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "username")
-    private String username;
+    private String username; // login
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")

8eb1f2e7c31c7cc8016ef536792f2d18ebf28319
Zamir <50745794+zmrbk@users.noreply.github.com>
2024-10-10 04:11:10 +0600

    added lesson entity

--- src/main/java/kg/megacom/diplomaprojectschoolmegalab/entity/Lesson.java
+++ src/main/java/kg/megacom/diplomaprojectschoolmegalab/entity/Lesson.java
@@ -3,6 +3,7 @@ package kg.megacom.diplomaprojectschoolmegalab.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Table(name = "lessons")
@@ -23,5 +24,7 @@ public class Lesson {
     @JoinColumn(name = "schedule_id", referencedColumnName = "id", nullable = false)
     private Schedule schedule;
     @Column(name = "creation_date", nullable = false)
-    private LocalDateTime creationDate;
+    private LocalDateTime creationDate = LocalDateTime.now();
+    @OneToMany(mappedBy = "lesson", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Mark> marks;
 }
--- src/main/java/kg/megacom/diplomaprojectschoolmegalab/service/LessonService.java
+++ src/main/java/kg/megacom/diplomaprojectschoolmegalab/service/LessonService.java
@@ -1,4 +1,14 @@
 package kg.megacom.diplomaprojectschoolmegalab.service;
 
+import kg.megacom.diplomaprojectschoolmegalab.dto.LessonDto;
+import kg.megacom.diplomaprojectschoolmegalab.dto.Response;
+
+import java.util.List;
+
 public interface LessonService {
+    void create(LessonDto lessonDto);
+    Response<List<LessonDto>> getAll();
+    LessonDto getById(Long id);
+    Response<LessonDto> update(Long id, LessonDto lessonDto);
+    void delete(Long id);
 }

e680909d2e47aefdb13cdee0d18ff6ab0a4579b2
Admin <armatov.jenisbek@gmail.com>
2024-10-10 00:29:38 +0600

    Code review

--- src/main/java/kg/megacom/diplomaprojectschoolmegalab/repository/ParentRepository.java
+++ src/main/java/kg/megacom/diplomaprojectschoolmegalab/repository/ParentRepository.java
@@ -1,6 +1,5 @@
 package kg.megacom.diplomaprojectschoolmegalab.repository;
 
-import kg.megacom.diplomaprojectschoolmegalab.entity.Employee;
 import kg.megacom.diplomaprojectschoolmegalab.entity.Parent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;

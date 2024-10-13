b5c866307c5d1d4b30e113b4094a331a83d7d1a5
Admin <armatov.jenisbek@gmail.com>
2024-10-13 20:34:43 +0600

    refactoring code

--- src/main/java/kg/megacom/diplomaprojectschoolmegalab/controller/ReviewController.java
+++ src/main/java/kg/megacom/diplomaprojectschoolmegalab/controller/ReviewController.java
@@ -5,6 +5,7 @@ import kg.megacom.diplomaprojectschoolmegalab.dto.ReviewDto;
 import kg.megacom.diplomaprojectschoolmegalab.exceptions.EntityNotFoundException;
 import kg.megacom.diplomaprojectschoolmegalab.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,9 +20,13 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @PostMapping
-    public ResponseEntity<Void> create(@RequestBody ReviewDto reviewDto) throws AccountNotFoundException {
-        reviewService.create(reviewDto);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<String> create(@RequestBody ReviewDto reviewDto) throws AccountNotFoundException {
+        try {
+            reviewService.create(reviewDto);
+            return ResponseEntity.ok().build();
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
     @PutMapping("/{id}")
--- src/main/java/kg/megacom/diplomaprojectschoolmegalab/service/SubjectService.java
+++ src/main/java/kg/megacom/diplomaprojectschoolmegalab/service/SubjectService.java
@@ -1,6 +1,5 @@
 package kg.megacom.diplomaprojectschoolmegalab.service;
 
-import kg.megacom.diplomaprojectschoolmegalab.dto.ParentDto;
 import kg.megacom.diplomaprojectschoolmegalab.dto.Response;
 import kg.megacom.diplomaprojectschoolmegalab.dto.SubjectsDto;
 import kg.megacom.diplomaprojectschoolmegalab.exceptions.EntityNotFoundException;
@@ -10,7 +9,7 @@ import java.util.List;
 
 public interface SubjectService {
     void create(SubjectsDto subjectsDto) throws AccountNotFoundException;
-    Response<SubjectsDto> update(SubjectsDto parentDto, Long id) throws AccountNotFoundException;
+    Response<SubjectsDto> update(SubjectsDto subjectsDto, Long id) throws AccountNotFoundException;
     Response<String> delete(Long id)throws EntityNotFoundException;
     Response<List<SubjectsDto>> getAll();
     Response<SubjectsDto> getById(Long id);
